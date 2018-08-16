e6b36cf20c0f7ab2014c5c9fc0433e4a1cccf249
ChGrossmann <ch.grossmann@gmx.ch>
2018-08-16 19:10:49 +0200

    AddTab funktion erstellen.

--- src/main/java/ch/teko/pa4/materialverwaltung/ui/AddTab.java
+++ src/main/java/ch/teko/pa4/materialverwaltung/ui/AddTab.java
@@ -29,18 +29,44 @@ public class AddTab {
     final GridLayout addArticleLayout = new GridLayout(2,6);
     
     
+    Label idLab = new Label("id");
+    TextField dTxt = new TextField("");
+    Label bahnLab = new Label("Bahn");
+    TextField bahnTxt = new TextField("");
+    Label linieLab = new Label("Linie");
+    TextField linieTxt = new TextField("");
     Label stationLab = new Label("Station");
-    TextField stationTxt = new TextField("Searchtext");
-    Label elementLab = new Label("Element");
-    TextField elementTxt = new TextField("Searchtext");
+    TextField stationTxt = new TextField("");
+    Label system1Lab = new Label("System I");
+    TextField stem1Txt = new TextField("");
+    Label system2Lab = new Label("System II");
+    TextField ystem2Txt = new TextField("");
+    Label system3Lab = new Label("System III");
+    TextField system3Txt = new TextField("");
     Label bezeichnungLab = new Label("Bezeichnung");
-    TextField bezeichnungTxt = new TextField("Searchtext");
+    TextField bezeichnungTxt = new TextField("");
+    Label typLab = new Label("Typ");
+    TextField typTxt = new TextField("");
+    Label beschreibungLab = new Label("Beschreibung");
+    TextField beschreibungTxt = new TextField("");
+    Label artikelNrLab = new Label("Art. Nr.");
+    TextField artikelNrTxt = new TextField("");
     Label anzahlLab = new Label("Anzahl");
-    TextField anzahlTxt = new TextField("Searchtext");
+    TextField anzahlTxt = new TextField("");
+    Label gestellLab = new Label("Gestell");
+    TextField gestellTxt = new TextField("");
+    Label schieneLab = new Label("Schiene");
+    TextField schieneTxt = new TextField("");
     Label schrankLab = new Label("Schrank");
-    TextField schrankTxt = new TextField("Searchtext");
+    TextField schrankTxt = new TextField("");
     Label tablarLab = new Label("Tablar");
-    TextField tablarTxt = new TextField("Searchtext");
+    TextField tablarTxt = new TextField("");
+    Label boxLab = new Label("Box");
+    TextField boxTxt = new TextField("");
+    Label bemerkungLab = new Label("Bemerkung");
+    TextField bemerkungTxt = new TextField("");
+    
+    
     
     addArticleLayout.addComponent(stationLab, 0, 0);
     addArticleLayout.addComponent(stationTxt, 1, 0);
